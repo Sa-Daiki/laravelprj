fb0034448e9c3f6546fa195e45b0368df15c83b8
Sato Daiki <sato-daiki@evolu.tokyo>
2021-12-13 11:46:41 +0900

    コメントアウトの削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,20 +8,14 @@ use App\Models\Comment;
 
 class PostController extends Controller
 {
-    // public function home() //メソッド名（なんでもいい）
-    // {
-    //     return view('posts/home', []);
-    // }
-
     public function mypost()
     {
         $user = \Auth::user();
-        // dd($user);
         if($user != null){
          $posts = Article:: where('user_id', $user['id'])
                                 ->where('status', 1)
                                 ->orderBy('updated_at', 'ASC')
-                                ->get(); //Memos tableからすべてのメモを持ってくるという指令
+                                ->get();
             return view('/posts/mypost', compact('user', 'posts'));
      }
         else{
@@ -31,13 +25,11 @@ class PostController extends Controller
 
     public function index()
     {
-        $user = \Auth::user(); //ログインしているユーザー情報//ログインしてない場合はnullが返る、ログインしていない場合も書いておく
-        // dd($user);
-        //投稿を取得
+        $user = \Auth::user();
         if($user != null){
             $posts = Article::orderBy('updated_at', 'ASC')
-                                ->take(20)  //上から5個表示させるようにしてる
-                                ->get(); //Memos tableからすべてのメモを持ってくるという指令
+                                ->take(20)
+                                ->get();
             return view('/posts', compact('user', 'posts'));
         }
         else{
@@ -45,27 +37,18 @@ class PostController extends Controller
         }
     }
 
-// $fruits = ["apple"=>"りんご","ぶどう","みかん"] //りんごが０キーふられてる　//[]のじてんでarray
-// $fruits[1] //ぶどうを取り出せてる（arrayの中のキーを指定することでValueが取り出せてる）
-//キーは自分で決めることもできる
-
-
-
      public function create()
     {
-        $user = \Auth::user(); //ログインしているユーザー情報をViewに渡している
+        $user = \Auth::user();
         return view('/posts/create', compact('user'));
     }
 
-    public function store(Request $request) //Requestはフォームに入力された値を受け取れる
+    public function store(Request $request)
     {
-            $data = $request -> all(); //送信、受信されたデータをすべて取る
-            // dd($data);//デバック関数
-            // dd($request->user()->id);
+            $data = $request -> all();
             $memo_id = Article::create([
                 'title'=>'title', 'content' => $data['content'], 'user_id' => $data['user_id']
             ]);
-            // 'content',''user_id,'status'は全部カラム名に一致している！！！！！！
             return redirect() -> route('posts.index');
     }
 
@@ -75,35 +58,28 @@ class PostController extends Controller
         if($user != null){
         $article = Article::where('id', $id)
                                       ->first();
-                                    //   dd($article);
         return view('posts/show', compact('user', 'article'));}
         else{
             return view('/posts');
         }
     }
 
-    public function edit($id) //$idは変数routingの{}に対応
+    public function edit($id)
     {
-        //該当するIDのメモをデータベースから取得
         $user = \Auth::user();
         $post = Article::where('status', 1)
                             ->where('id', $id)
                             ->where('user_id', $user['id'])
-                            ->first();//条件に該当した行を1行だけ取ってくる
-                            // dd($post);
+                            ->first();
         $posts = Article::where('user_id', $user['id'])
                                 ->where('status', 1)
                                 ->orderBy('updated_at', 'DESC')
-                                ->take(20)  //上から5個表示させるようにしてる
-                                ->get(); //Memos tableからすべてのメモを持ってくるという指令
-        //取得したメモをViewに渡す
-        // $tags = Tag::where('user_id', $user['id'])->get();
+                                ->take(20)
+                                ->get();
         return view('/posts/edit', compact('user', 'post', 'posts'));
-    // posts.blade.phpでは全ての投稿が見れて、そこから詳細ページ(show.blade.php)に飛ぶようにする
-    // posts.blade.phpでは全ての投稿が見れて、そこから詳細ページ(show.blade.php)に飛ぶようにする
     }
 
-    public function update(Request $request, $id)//$id使うことでパラメータの後ろの数字取れる
+    public function update(Request $request, $id)
     {
         $inputs = $request -> all();
         Article::where('id', $id)
@@ -111,21 +87,11 @@ class PostController extends Controller
         return redirect() -> route('posts.index');
     }
 
-    public function destroy(Request $request, $id)//$id使うことでパラメータの後ろの数字取れる
-    {
-        // dd("あ");
+    public function destroy(Request $request, $id){
         $inputs = $request -> all();
         Article::where('id', $id)
-                ->delete(); //これは物理的に削除する方法、論理削除はなるべくしない
+        ->delete();
         return redirect() -> route('posts.index');
     }
 
 };
-
-// viewに送ってないから映らない
-
-//posts/storeじゃなくて/storeにとぶようにすればよき
-
-// showとindexの違い調べる
-// 逆かもしれない
-//ログインしている場合としてない場合の条件文書いておく
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,13 +8,8 @@
     </textarea>
     <button type='submit'>更新</button>
 </form>
-{{-- {{dd($post)}} --}}
 <form method='POST' action="/posts/{{$post['id']}}">
     @csrf
     @method('delete')
     <button type='submit'>削除</button>
-    {{-- @extends('layouts.layout') --}}
 </form>
-
-{{-- フォームで投げるためには名前を付ける必要がある
-問題点を明確にする　英語で調べれば出てくることが多い --}}
